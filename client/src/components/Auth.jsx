5a7473e0d649b8890ac91a9d39c869e4d86454a6
Indranil Ray <rayindranil29@gmail.com>
2024-11-17 02:01:33 +0530

    Some styling changes in Auth Pages

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,8 +4,26 @@ import "../styles/Auth.css"
 
 const Auth = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-8 mt-10'>
-      <h1 className='text-white text-5xl border-4 border-green-600 p-4'>Welcome to the Quiz Application</h1>
+    <div 
+    style={{
+      "display": "flex",
+      "flexDirection": "column",
+      "justifyContent": "center",
+      "alignItems": "center",
+      "gap": "2rem",
+      "marginTop": "2.5rem",
+    }}>
+      <h1 className='text-white text-5xl border-4 border-green-600 p-4'
+      style={{
+        "color": "white",
+        "fontSize": "3rem",
+        "lineHeight": "1",
+        "border": "4px solid #16a34a",
+        // "borderWidth": "4px",
+        "borderColor": "#16a34a",
+        "padding": "10px"
+      }}
+      >Welcome to the Quiz Application</h1>
       <SignIn routing="path" path="/sign-in" />
       <SignUp routing="path" path="/sign-up" />
     </div>
